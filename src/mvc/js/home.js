cbe4dff121a5071c4bd18a9ea08a422701f47bf1
nabab <thomas.nabet@gmail.com>
2018-03-11 17:08:19 +0100

    Added dynamic component loading rules

--- src/mvc/js/home.js
+++ src/mvc/js/home.js
@@ -1,6 +1,19 @@
 // Javascript Document
 (() => {
   return {
+    beforeMount(){
+      this.source.data.forEach((w,) => {
+        let comp = w.component || w.itemComponent;
+        if ( comp && !Vue.options.components[comp] ){
+          let  c = comp.split('-'),
+               prefix = c.shift();
+          c = c.join('/');
+          bbn.vue.setComponentRule('components/', prefix);
+          bbn.vue.addComponent(c);
+          bbn.vue.unsetComponentRule();
+        }
+      });
+    },
     data(){
       return {
         widgets: this.source.data
