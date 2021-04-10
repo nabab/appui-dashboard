a1bc65fbb0a01b63f288532ef83852a491f6affd
Thomas Nabet <thomas.nabet@gmail.com>
2021-04-10 18:10:46 +0200

    Bugfix in _ctrl

--- src/mvc/public/_ctrl.php
+++ src/mvc/public/_ctrl.php
@@ -3,13 +3,23 @@
 $ok = true;
 if ( !defined('APPUI_DASHBOARD_ROOT') ){
   define('APPUI_DASHBOARD_ROOT', $ctrl->pluginUrl('appui-dashboard').'/');
-  $id = $ctrl->post['id_dashboard'] ?? ($ctrl->arguments[0] ?? 'default');
+  if (!empty($ctrl->post['id_dashboard'])) {
+    $id = $ctrl->post['id_dashboard'];
+  }
+  elseif ($ctrl->hasArguments() && ($ctrl->getPath() === 'home')) {
+    $id = $ctrl->arguments[0];
+  }
+  else {
+    $id = 'default';
+  }
+
   try {
     $ctrl->addInc('dashboard', new \bbn\Appui\Dashboard($id));
   }
-  catch ( Exception $e ){
+  catch (Exception $e) {
     $ctrl->obj->error = $e->getMessage();
     $ok = false;
   }
 }
+
 return $ok;
