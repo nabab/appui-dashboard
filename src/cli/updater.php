536ce8e16320f23f052cbf7b0ed8148d3d5c5b43
Mirko Argentino <mirko@bbn.solutions>
2022-05-30 17:17:50 +0200

    Added updated cli

--- src/cli/updater.php
+++ src/cli/updater.php
@@ -7,6 +7,6 @@ X::adump('Hello from the updater...');
 $appui = new \bbn\Appui();
 $pref = new \bbn\User\Preferences($appui->getDb());
 $perm = new \bbn\User\Permissions();
-if ($res = $appui->updateMenus()) {
-  echo $res . _('menus changed');
+if ($res = $appui->updateDashboard()) {
+  echo $res . _('changes made') . PHP_EOL;
 }
