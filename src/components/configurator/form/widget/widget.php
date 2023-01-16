cce8d1d6697266c824dedbb002821424cf928a32
nabab <thomas.nabet@gmail.com>
2023-01-16 12:06:47 +0100

    Fix in widget form

--- src/components/configurator/form/widget/widget.php
+++ src/components/configurator/form/widget/widget.php
@@ -12,35 +12,50 @@
                     :nullable="true"
                     :source="plugins"/>
     </template>
+
     <label><?=_('Name')?></label>
     <bbn-input v-model="source.row[configurator.optCfg.fields.text]"
                :required="true"/>
+
     <label><?=_('Code')?></label>
     <bbn-input v-model="source.row[configurator.optCfg.fields.code]"
                :required="true"/>
+
+    <label><?=_('URL')?></label>
+    <bbn-input v-model="source.row.url"/>
+
     <label><?=_('Icon')?></label>
     <bbn-input v-model="source.row.icon"/>
+
     <label><?=_('Closable')?></label>
     <bbn-checkbox v-model="source.row.closable"
                   :value="true"
                   :novalue="false"/>
+
     <label><?=_('Observe')?></label>
     <bbn-checkbox v-model="source.row.observe"
                   :value="true"
                   :novalue="false"/>
+
     <label><?=_('Limit')?></label>
     <bbn-numeric v-model="source.row.limit"
                  :min="0"/>
+
     <label><?=_('Component')?></label>
     <bbn-input v-model="source.row.component"/>
+
     <label><?=_('itemComponent')?></label>
     <bbn-input v-model="source.row.itemComponent"/>
+
     <label><?=_('Right buttons')?></label>
     <bbn-json-editor v-model="source.row.buttonsRight"/>
+
     <label><?=_('Left buttons')?></label>
     <bbn-json-editor v-model="source.row.buttonsLeft"/>
+
     <label><?=_("Component's options")?></label>
     <bbn-json-editor v-model="source.row.options"/>
+
     <label><?=_('Cache')?></label>
     <bbn-numeric v-model="source.row.cache"
                 :min="0"/>
