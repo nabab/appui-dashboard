9d02dea761b0cd138eb193c097ab3033c028bf85
Mirko Argentino <mirko@bbn.solutions>
2025-09-30 16:18:35 +0200

    Bugfixes on _super ctrl

--- src/mvc/public/_super.php
+++ src/mvc/public/_super.php
@@ -1,38 +1,44 @@
 <?php
 use bbn\X;
+use bbn\Appui\Dashboard;
 
 /** @var bbn\Mvc\Controller $ctrl */
 $ok = true;
 if (!defined('APPUI_DASHBOARD_ROOT')) {
   define('APPUI_DASHBOARD_ROOT', $ctrl->pluginUrl('appui-dashboard').'/');
-  $ctrl->addInc('dashboard', new \bbn\Appui\Dashboard());
-  if (!empty($ctrl->post['id_dashboard'])) {
-    $id = $ctrl->post['id_dashboard'];
-  }
-  elseif ($ctrl->hasArguments() && ($ctrl->getPath() === 'home')) {
-    $id = $ctrl->arguments[0];
-  }
-  else {
-    if (!$id = $ctrl->inc->dashboard->getDefault()) {
-      $dashboards = $ctrl->inc->dashboard->getUserDashboards();
-      if (X::getRow($dashboards, ['code' => 'default'])) {
-        $id = 'default';
-      }
-      elseif (!empty($dashboards)) {
-        $id = $dashboards[0]['id'];
-      }  
-    }
-  }
+}
 
-  if ($id) {
-    try {
-      $ctrl->inc->dashboard->setCurrent($id);
+if (!isset($ctrl->inc->dashboard)) {
+  $ctrl->addInc('dashboard', new Dashboard());
+}
+
+if (!empty($ctrl->post['id_dashboard'])) {
+  $id = $ctrl->post['id_dashboard'];
+}
+elseif ($ctrl->hasArguments() && ($ctrl->getPath() === 'home')) {
+  $id = $ctrl->arguments[0];
+}
+else {
+  if (!$id = $ctrl->inc->dashboard->getDefault()) {
+    $dashboards = $ctrl->inc->dashboard->getUserDashboards();
+    if (X::getRow($dashboards, ['code' => 'default'])) {
+      $id = 'default';
     }
-    catch (Exception $e) {
-      $ctrl->obj->error = $e->getMessage();
-      $ok = false;
+    elseif (!empty($dashboards)) {
+      $id = $dashboards[0]['id'];
     }
   }
 }
+
+if ($id) {
+  try {
+    $ctrl->inc->dashboard->setCurrent($id);
+  }
+  catch (Exception $e) {
+    $ctrl->obj->error = $e->getMessage();
+    $ok = false;
+  }
+}
+
 
 return $ok;
